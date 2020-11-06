eac2ce075f14db3f3afdf5be640666f7619f3d4f
ninjha01 <ninjha01@gmail.com>
2020-11-06 09:19:38 -0500

    Setup for installing via pip from github

--- setup.py
+++ setup.py
@@ -2,5 +2,8 @@ from setuptools import setup, Extension
 from Cython.Build import cythonize
 import numpy
 
-# Need to export CFLAGS="-I /usr/local/lib/python3.7/site-packages/numpy/core/include/ $CFLAGS"
-setup(ext_modules=cythonize("pearson.pyx", include_path=[numpy.get_include()]))
+setup(
+    ext_modules=cythonize(
+        [Extension("pearson", ["src/pearson.pyx"], include_dirs=[numpy.get_include()])]
+    )
+)
